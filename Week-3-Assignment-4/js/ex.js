9c48d8d086a7d005626ec6e12e252a4fd3d11eda
omartohme <tohm0011@algonquinlive.com>
2015-09-28 19:54:47 -0400

    code

--- Week-3-Assignment-4/js/ex.js
+++ Week-3-Assignment-4/js/ex.js
@@ -1,3 +1,34 @@
-// JavaScript Document
+var boxRed;
+var boxBlue;
+var mouseover = false;
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init (){
+   
+   boxRed = document.querySelector(".long-rect");
+   boxBlue= document.querySelector(".short-rect");
+   boxRed.addEventListener("mouseover", onMouseover);
+   boxRed.addEventListener("mouseout", onMouseout);
+   boxBlue.addEventListener("click", onClick);
+}
+
+function onMouseover() {
+	boxBlue.innerText = "you can click";
+	boxRed.innerText = "You moused over the big box";
+	mouseover = true;
+}
+
+function onClick(){
+	
+	if (mouseover){
+		alert("you clicked it"); 
+		
+	}
+}
+
+function onMouseout() {
+	boxRed.innerText = "Your mouse is out";
+}
 
 
